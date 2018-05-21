94f6327c856c555a06c67a58e038bac3c99dff6c
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-05-21 13:21:32 -0700

    injected the HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[]; 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
