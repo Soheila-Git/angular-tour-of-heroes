cea83ff74bc54ba5409fc5feee7b355c65580f9f
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-05-30 13:19:07 -0700

    extract the id route parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -19,6 +19,12 @@ export class HeroDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    
   }
 
 }
